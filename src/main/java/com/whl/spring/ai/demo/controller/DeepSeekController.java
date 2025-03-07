4d3fe9f5e411db01eb5a37ecd9b9610ec3392d8f
王洪亮 <w505957125@aliyun.com>
2025-03-07 13:57:37 +0800

    first commit

---
+++ src/main/java/com/whl/spring/ai/demo/controller/DeepSeekController.java
@@ -0,0 +1,40 @@
+package com.whl.spring.ai.demo.controller;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/deepseek")
+public class DeepSeekController {
+    private final OpenAiChatModel chatModel;
+
+    public DeepSeekController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "Hello, I'm DeepSeek R1!";
+    }
+
+    @GetMapping("/ai/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "给我讲个笑话") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "给我讲个笑话") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+
+}
