c7e2530069199218d164cc26167b4ea997563b95
王洪亮 <w505957125@aliyun.com>
2025-03-10 14:17:00 +0800

    1. add ollama demo 2. add rag url demo

---
+++ src/main/java/com/whl/spring/ai/demo/controller/OllamaController.java
@@ -0,0 +1,52 @@
+package com.whl.spring.ai.demo.controller;
+
+import org.springframework.ai.autoconfigure.ollama.OllamaConnectionProperties;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ollama")
+public class OllamaController {
+    private final OllamaConnectionProperties properties;
+
+    private final OllamaApi ollamaApi;
+
+    public OllamaController(OllamaConnectionProperties properties, OllamaApi ollamaApi) {
+        this.properties = properties;
+        this.ollamaApi = ollamaApi;
+    }
+
+    @GetMapping("")
+    public String index() {
+        return "Hello, I'm Ollama!";
+    }
+
+    @GetMapping("/listModels")
+    public OllamaApi.ListModelResponse listModels() {
+        return this.ollamaApi.listModels();
+    }
+
+    @GetMapping("/ai/generate")
+    public ChatResponse generate(@RequestParam String model, @RequestParam String message) {
+        OpenAiChatModel chatModel = OpenAiChatModel.builder()
+                .openAiApi(OpenAiApi.builder()
+                        .baseUrl(this.properties.getBaseUrl())
+                        .build())
+                .defaultOptions(OpenAiChatOptions.builder()
+                        .model(model)
+                        .build())
+                .build();
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return chatModel.call(prompt);
+    }
+
+}
--- src/main/java/com/whl/spring/ai/demo/controller/RagController.java
+++ src/main/java/com/whl/spring/ai/demo/controller/RagController.java
@@ -1,19 +1,22 @@
 package com.whl.spring.ai.demo.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.autoconfigure.vectorstore.redis.RedisVectorStoreProperties;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
-import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 import redis.clients.jedis.JedisPooled;
 
-import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,15 +29,23 @@ public class RagController {
 
     private final RedisVectorStoreProperties redisVectorStoreProperties;
 
+    private final WebClient webClient;
+
+    private final ObjectMapper objectMapper;
+
     private final OpenAiChatModel chatModel;
 
     public RagController(VectorStore vectorStore,
                          JedisPooled jedis,
                          RedisVectorStoreProperties redisVectorStoreProperties,
+                         WebClient.Builder builder,
+                         ObjectMapper mapper,
                          OpenAiChatModel chatModel) {
         this.vectorStore = vectorStore;
         this.jedis = jedis;
         this.redisVectorStoreProperties = redisVectorStoreProperties;
+        this.webClient = builder.build();
+        this.objectMapper = mapper;
         this.chatModel = chatModel;
     }
 
@@ -45,12 +56,24 @@ public class RagController {
 
     @GetMapping("/add")
     public String add() throws Exception {
-        File file = new File("C:\\Users\\Administrator\\Desktop\\Linux操作系统查看服务器出口IP地址.docx");
-        TikaDocumentReader reader = new TikaDocumentReader(new FileSystemResource(file));
-        TokenTextSplitter splitter = new TokenTextSplitter(300, 200, 10, 400, true);
-        List<Document> documents = splitter.apply(reader.get());
+//        File file = new File("C:\\Users\\Administrator\\Desktop\\Linux操作系统查看服务器出口IP地址.docx");
+//        TikaDocumentReader reader = new TikaDocumentReader(new FileSystemResource(file));
+//        TokenTextSplitter splitter = new TokenTextSplitter(300, 200, 10, 400, true);
+//        List<Document> documents = splitter.apply(reader.get());
+//        documents.forEach(document -> {
+//            document.getMetadata().put("id", document.getId());
+//            vectorStore.add(List.of(document));
+//        });
+        String json = this.webClient.get().uri("https://yq.weaver.com.cn/eb/qa/question/queryById/ed9368785682414785fbe82a5a2bb8c2").retrieve().bodyToMono(String.class).block();
+        JsonNode jsonNode = this.objectMapper.readValue(json, JsonNode.class);
+        JsonNode result = jsonNode.get("result");
+        String title = result.get("title").asText();
+        String content = result.get("content").asText();
+        Resource resource = new ByteArrayResource(content.getBytes(StandardCharsets.UTF_8));
+        TikaDocumentReader reader = new TikaDocumentReader(resource);
+        List<Document> documents = reader.get();
         documents.forEach(document -> {
-            document.getMetadata().put("id", document.getId());
+            document.getMetadata().put("title", title);
             vectorStore.add(List.of(document));
         });
         return "SUCCESS";
